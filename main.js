31c3a2627f6974dda6d0d21feb79d05b9c6ec496
unknown <noeliacondemaita0@gmail.com>
2023-12-12 08:15:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var audio = document.getElementById('audio');
+var playPauseBtn = document.getElementById('playPauseBtn');
+var progressBar = document.getElementById('progressBar');
+var currentTimeElement = document.getElementById('currentTime');
+var totalTimeElement = document.getElementById('totalTime');
+var albumCover = document.getElementById('albumCover');
+
+function togglePlayPause() {
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.textContent = '🤍';
+        albumCover.style.animation = 'rotation 5s infinite linear';
+    } else {
+        audio.pause();
+        playPauseBtn.textContent = '❤';
+        albumCover.style.animation = 'none';
+    }
+}
+
+
